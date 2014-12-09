15b86228a78c80c20c0fe4e0d7f47b626d2fbd86
lys <liuyanshan01060@pwrd.com>
2014-12-09 16:50:46 +0800

    test

--- statcode.sh
+++ statcode.sh
@@ -1,74 +1,47 @@
 #!/bin/bash
 
-    beginday=$2
-    endday=$3
-   
-
 function count() {
-    local insert=0
-    local delete=0
-    while read line ;do
-        current=`echo $line| awk -F',' '{printf $2}' | awk '{printf $1}'`
-        if [[ -n $current ]]; then 
-            insert=`expr $insert + $current`
-        fi
-        current=`echo $line | sed -n 's/.*, //p' | awk '{printf $1}'`
-        if [[ -n $current ]]; then
-            delete=`expr $delete + $current`
-        fi
-    done < .tmp.count
-    echo "$insert insertions, $delete deletions"
+	local insert=0
+	local delete=0
+	while read line ;do
+		current=`echo $line| awk -F',' '{printf $2}' | awk '{printf $1}'`
+		if [[ -n $current ]]; then 
+			insert=`expr $insert + $current`
+		fi
+		current=`echo $line | sed -n 's/.*, //p' | awk '{printf $1}'`
+		if [[ -n $current ]]; then
+			delete=`expr $delete + $current`
+		fi
+	done < .tmp.count
+	echo "$insert insertions, $delete deletions"
 }
 
-function countAll() {
-    for user  in `git log --pretty='%aN' | sort -u`;do
-         git log --author=$user --shortstat --pretty=format:"" | sed /^$/d >.tmp.count
-         count;
-         rm .tmp.count
-   done
-}
+action=$1
+beginday=$2
+endday=$3
 
-function countToday() {
-    local current=`date +%s`;
-    local begin=`date +%Y-%m-%d |xargs date +%s -d`;
-    local minutes=$(($current - $begin));
-
-    for user  in `git log --pretty='%aN' | sort -u`;do
-	    git log --author=$user --since="$minutes seconds ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count
-	    count;
-	    rm .tmp.count
-	    done
-
-}
-
-function countOneDay() {
-    for user  in `git log --pretty='%aN' | sort -u`;do
-	    git log --author=liuyanshan --since="1 days ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count
-	    count;
-	    rm .tmp.count
-	done
-
-}
-
-function countDayToDay() {
-    for user  in `git log --pretty='%aN' | sort -u`;do
-	    git log --author=$user --since="$beginday days ago" --before="$endday days ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count
-	    count;
-	    rm .tmp.count
-	done
-
-}
+users=($(git log --pretty='%aN' | sort -u))
+current=`date +%s`;
+begin=`date +%Y-%m-%d |xargs date +%s -d`;
+minutes=$(($current - $begin));
+cmd='';
 
 
-if [[ ! -n $1 ]] || [[ $1 = "all" ]] ; then 
-    countAll;
-elif [[ $1 = "oneday" ]]; then
-    countOneDay;
-elif [[ $1 = "today" ]]; then
-    countToday;
-elif [[ $1 = "daytoday" ]]; then
-    countDayToDay;
-else
-    echo "args: all | oneday | today | daytoday";
-fi
+for user  in ${users[@]};do
 
+	if [[ ! -n $action ]] || [[ $action = "all" ]] ; then 
+		cmd=`git log --author=$user --shortstat --pretty=format:"" | sed /^$/d >.tmp.count`
+	elif [[ $action = "oneday" ]]; then
+		cmd=`git log --author=$user --since="1 days ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count`
+	elif [[ $action = "today" ]]; then
+		cmd=`git log --author=$user --since="$minutes seconds ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count`
+	elif [[ $action = "daytoday" ]]; then
+		cmd=`git log --author=$user --since="$beginday days ago" --before="$endday days ago" --shortstat --pretty=format:"" | sed /^$/d >.tmp.count`
+	else
+		echo "args: all | oneday | today | daytoday";
+		exit -1;
+	fi
+	echo $user;
+	count;
+	rm .tmp.count
+done
